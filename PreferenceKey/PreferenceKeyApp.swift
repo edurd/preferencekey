b2895709a232582c02ea164dab41046f3640b1cb
Eduardo Hoyos <edu.hoyos92@gmail.com>
2025-04-16 10:00:26 -0500

    Initial Commit

---
+++ PreferenceKey/PreferenceKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferenceKeyApp.swift
+//  PreferenceKey
+//
+//  Created by Eduardo Hoyos on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PreferenceKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
